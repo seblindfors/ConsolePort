c808e590362b0ba19e7d7b2dbefc032abed5ecad
seblindfors <sl92cb@gmail.com>
2015-03-06 17:40:20 +0100

    Alpha 0.1.14
    - Stack split frame behaviour overhauled.
    - Fixed "Buy different amount" appearing on all items bug.
    - Optimized some logic.

--- ConsolePort/ConsolePortShop.lua
+++ ConsolePort/ConsolePortShop.lua
@@ -18,50 +18,52 @@ function ConsolePort:Shop(key, state)
 	local slot = items[iterator];
 	local index = slot:GetID();
 	if key == G.PREPARE then iterator = 1;
-	elseif key == G.TRIANGLE and state == G.STATE_DOWN then
-		local maxStack = GetMerchantItemMaxStack(slot:GetID());
-		local _, _, price, stackCount, _, _, extendedCost = GetMerchantItemInfo(slot:GetID());
-		if stackCount > 1 and extendedCost then
-			slot:Click();
-			return;
-		end
-		local canAfford;
-		if 	price and price > 0 then
-			canAfford = floor(GetMoney() / (price / stackCount));
-		else
-			canAfford = maxStack;
-		end
-		if	maxStack > 1 then
-			local maxPurchasable = min(maxStack, canAfford);
-			Leave(slot);
-			OpenStackSplitFrame(maxPurchasable, slot, "BOTTOMLEFT", "TOPLEFT");
-		end
-	elseif 		state == G.STATE_DOWN then
-		local 	old = iterator;
-		if 		key == G.UP 	then iterator = iterator - 1;
-		elseif	key == G.DOWN 	then iterator = iterator + 1;
-		elseif 	key == G.RIGHT 	then iterator = iterator + 5;
-		elseif 	key == G.LEFT 	then iterator = iterator - 5; end;
-		if iterator > 10 and MerchantNextPageButton:IsEnabled() and MerchantNextPageButton:IsVisible() then
-			MerchantNextPageButton:Click();
-			iterator = old - 5;
-		elseif iterator < 1 and MerchantPrevPageButton:IsEnabled() and MerchantPrevPageButton:IsVisible() then
-			MerchantPrevPageButton:Click();
-			iterator = old + 5;
-		elseif iterator < 1 or iterator > 10 then
-			iterator = old;
-		end
-		if not items[iterator].hasItem then
-			iterator = 1;
-		end
-		slot = items[iterator];
-		ConsolePort:Highlight(iterator, items);
-		Enter(slot);
-		if 	key == G.CIRCLE then
+	elseif state == G.STATE_DOWN then
+		if key == G.CIRCLE then
 			slot:Click("RightButton");
 		elseif key == G.SQUARE then
 			MouselookStop();
+			OpenAllBags();
 			PickupMerchantItem(index);
+		elseif key == G.TRIANGLE then
+			local maxStack = GetMerchantItemMaxStack(slot:GetID());
+			local _, _, price, stackCount, _, _, extendedCost = GetMerchantItemInfo(slot:GetID());
+			if stackCount > 1 and extendedCost then
+				slot:Click();
+				return;
+			end
+			local canAfford;
+			if 	price and price > 0 then
+				canAfford = floor(GetMoney() / (price / stackCount));
+			else
+				canAfford = maxStack;
+			end
+			if	maxStack > 1 then
+				local maxPurchasable = min(maxStack, canAfford);
+				Leave(slot);
+				OpenStackSplitFrame(maxPurchasable, slot, "BOTTOMLEFT", "TOPLEFT");
+			end
+		else
+			local 	old = iterator;
+			if 		key == G.UP 	then iterator = iterator - 1;
+			elseif	key == G.DOWN 	then iterator = iterator + 1;
+			elseif 	key == G.RIGHT 	then iterator = iterator + 5;
+			elseif 	key == G.LEFT 	then iterator = iterator - 5; end;
+			if iterator > 10 and MerchantNextPageButton:IsEnabled() and MerchantNextPageButton:IsVisible() then
+				MerchantNextPageButton:Click();
+				iterator = old - 5;
+			elseif iterator < 1 and MerchantPrevPageButton:IsEnabled() and MerchantPrevPageButton:IsVisible() then
+				MerchantPrevPageButton:Click();
+				iterator = old + 5;
+			elseif iterator < 1 or iterator > 10 then
+				iterator = old;
+			end
+			if not items[iterator].hasItem then
+				iterator = 1;
+			end
+			slot = items[iterator];
+			ConsolePort:Highlight(iterator, items);
+			Enter(slot);
 		end
 	end
 end
