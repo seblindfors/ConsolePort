b78e8405b6b9661d77ebc18970dece80c1523c08
seblindfors <sl92cb@gmail.com>
2015-11-24 19:06:35 +0100

    Beta 0.9.2
    - Fixed version metadata.
    - Removed unintentional global.

--- ConsolePort/Frames/CursorRaid.lua
+++ ConsolePort/Frames/CursorRaid.lua
@@ -4,198 +4,195 @@ local FadeOut = db.UIFrameFadeOut
 local UIHandle
 
 function ConsolePort:CreateRaidCursor()
-	if not dw then
-		UIHandle = CreateFrame("Frame", addOn.."UIHandle", ConsolePort, "SecureHandlerBaseTemplate")
-		local Key = {
-			Up 		= self:GetUIControlKey("CP_L_UP"),
-			Down 	= self:GetUIControlKey("CP_L_DOWN"),
-			Left 	= self:GetUIControlKey("CP_L_LEFT"),
-			Right 	= self:GetUIControlKey("CP_L_RIGHT"),
-		}
+	UIHandle = CreateFrame("Frame", addOn.."UIHandle", ConsolePort, "SecureHandlerBaseTemplate")
+	local Key = {
+		Up 		= self:GetUIControlKey("CP_L_UP"),
+		Down 	= self:GetUIControlKey("CP_L_DOWN"),
+		Left 	= self:GetUIControlKey("CP_L_LEFT"),
+		Right 	= self:GetUIControlKey("CP_L_RIGHT"),
+	}
 
-		UIHandle:Execute(format([[
-			Key = newtable()
-			Key.Up = %s
-			Key.Down = %s
-			Key.Left = %s
-			Key.Right = %s
- 			FrameStack = newtable()
- 			Bindings = newtable()
- 			Nodes = newtable()
-		]], Key.Up, Key.Down, Key.Left, Key.Right))
+	UIHandle:Execute(format([[
+		Key = newtable()
+		Key.Up = %s
+		Key.Down = %s
+		Key.Left = %s
+		Key.Right = %s
+			FrameStack = newtable()
+			Bindings = newtable()
+			Nodes = newtable()
+	]], Key.Up, Key.Down, Key.Left, Key.Right))
 
-		UIHandle:Execute([[
-			GetNodes = [=[
-				local node = CurrentNode
-				local children = newtable(node:GetChildren())
-				if not node:IsObjectType("Slider") then
-					for i, child in pairs(children) do
-						CurrentNode = child
-						self:Run(GetNodes)
-					end
+	UIHandle:Execute([[
+		GetNodes = [=[
+			local node = CurrentNode
+			local children = newtable(node:GetChildren())
+			if not node:IsObjectType("Slider") then
+				for i, child in pairs(children) do
+					CurrentNode = child
+					self:Run(GetNodes)
 				end
-				if node:GetAttribute("unit") then
-					local left, bottom, width, height = node:GetRect()
-					if left and bottom then
-						tinsert(Nodes, node)
-					end
+			end
+			if node:GetAttribute("unit") then
+				local left, bottom, width, height = node:GetRect()
+				if left and bottom then
+					tinsert(Nodes, node)
 				end
-			]=]
-		]])
-		UIHandle:Execute([[
-			SetCurrent = [=[
-				if old and old:IsVisible() then
-					current = old
-				elseif (not current and Nodes[1]) or (current and Nodes[1] and not current:IsVisible()) then
-					current = Nodes[1]
-				end
-			]=]
-		]])
-		UIHandle:Execute([[
-			FindClosestNode = [=[
-				if current then
-					local left, bottom, width, height = current:GetRect()
-					local thisY = bottom+height/2
-					local thisX = left+width/2
-					local nodeY = 10000
-					local nodeX = 10000
-					local swap 	= false
-					local destY, destX, diffY, diffX, total
-					for i, destination in ipairs(Nodes) do
-						if destination:IsVisible() then
-							local left, bottom, width, height = destination:GetRect()
-							destY = bottom+height/2
-							destX = left+width/2
-							diffY = abs(thisY-destY)
-							diffX = abs(thisX-destX)
-							total = diffX + diffY
-							if total < nodeX + nodeY then
-								if 	key == Key.Up then
-									if 	diffY > diffX and 	-- up/down
-										destY > thisY then 	-- up
-										swap = true
-									end
-								elseif key == Key.Down then
-									if 	diffY > diffX and 	-- up/down
-										destY < thisY then 	-- down
-										swap = true
-									end
-								elseif key == Key.Left then
-									if 	diffY < diffX and 	-- left/right
-										destX < thisX then 	-- left
-										swap = true
-									end
-								elseif key == Key.Right then
-									if 	diffY < diffX and 	-- left/right
-										destX > thisX then 	-- right
-										swap = true
-									end
+			end
+		]=]
+	]])
+	UIHandle:Execute([[
+		SetCurrent = [=[
+			if old and old:IsVisible() then
+				current = old
+			elseif (not current and Nodes[1]) or (current and Nodes[1] and not current:IsVisible()) then
+				current = Nodes[1]
+			end
+		]=]
+	]])
+	UIHandle:Execute([[
+		FindClosestNode = [=[
+			if current then
+				local left, bottom, width, height = current:GetRect()
+				local thisY = bottom+height/2
+				local thisX = left+width/2
+				local nodeY = 10000
+				local nodeX = 10000
+				local swap 	= false
+				local destY, destX, diffY, diffX, total
+				for i, destination in ipairs(Nodes) do
+					if destination:IsVisible() then
+						local left, bottom, width, height = destination:GetRect()
+						destY = bottom+height/2
+						destX = left+width/2
+						diffY = abs(thisY-destY)
+						diffX = abs(thisX-destX)
+						total = diffX + diffY
+						if total < nodeX + nodeY then
+							if 	key == Key.Up then
+								if 	diffY > diffX and 	-- up/down
+									destY > thisY then 	-- up
+									swap = true
+								end
+							elseif key == Key.Down then
+								if 	diffY > diffX and 	-- up/down
+									destY < thisY then 	-- down
+									swap = true
+								end
+							elseif key == Key.Left then
+								if 	diffY < diffX and 	-- left/right
+									destX < thisX then 	-- left
+									swap = true
+								end
+							elseif key == Key.Right then
+								if 	diffY < diffX and 	-- left/right
+									destX > thisX then 	-- right
+									swap = true
 								end
 							end
-							if swap then
-								nodeX = diffX
-								nodeY = diffY
-								current = destination
-								swap = false
-							end
+						end
+						if swap then
+							nodeX = diffX
+							nodeY = diffY
+							current = destination
+							swap = false
 						end
 					end
 				end
-			]=]
-		]])
-		UIHandle:Execute([[
-			SelectNode = [=[
-				key = ...
-				if current then
-					old = current
-				end
+			end
+		]=]
+	]])
+	UIHandle:Execute([[
+		SelectNode = [=[
+			key = ...
+			if current then
+				old = current
+			end
 
-				self:Run(SetCurrent)
-				self:Run(FindClosestNode)
+			self:Run(SetCurrent)
+			self:Run(FindClosestNode)
 
-				if current then
-					self:SetAttribute("node", current)
+			if current then
+				self:SetAttribute("node", current)
+			end
+		]=]
+	]])		
+	UIHandle:Execute([[
+		UpdateFrameStack = [=[
+			Nodes = newtable()
+			local children = newtable(self:GetFrameRef("UIParent"):GetChildren())
+			for i, Frame in pairs(children) do
+				if Frame:IsVisible() or Frame:IsProtected() then
+					CurrentNode = Frame
+					self:Run(GetNodes)
 				end
-			]=]
-		]])		
-		UIHandle:Execute([[
-			UpdateFrameStack = [=[
-				Nodes = newtable()
-				local children = newtable(self:GetFrameRef("UIParent"):GetChildren())
-				for i, Frame in pairs(children) do
-					if Frame:IsVisible() or Frame:IsProtected() then
-						CurrentNode = Frame
-						self:Run(GetNodes)
-					end
+			end
+		]=]
+	]])
+	UIHandle:Execute([[
+		ToggleCursor = [=[
+			local state = ...
+			if state then
+				for binding, name in pairs(Bindings) do
+					local key = GetBindingKey(binding)
+					self:SetBindingClick(true, key, "ConsolePortRaidCursorButton"..name)
 				end
-			]=]
-		]])
-		UIHandle:Execute([[
-			ToggleCursor = [=[
-				local state = ...
-				if state then
-					for binding, name in pairs(Bindings) do
-						local key = GetBindingKey(binding)
-						self:SetBindingClick(true, key, "ConsolePortRaidCursorButton"..name)
-					end
-					self:Run(UpdateFrameStack)
-					self:Run(SelectNode, 0)
-				else
-					self:SetAttribute("node", nil)
-					self:ClearBindings()
-				end
-			]=]
-		]])
+				self:Run(UpdateFrameStack)
+				self:Run(SelectNode, 0)
+			else
+				self:SetAttribute("node", nil)
+				self:ClearBindings()
+			end
+		]=]
+	]])
 
-		UIHandle:SetFrameRef("UIParent", UIParent)
-		UIHandle:Execute([[
-			CurrentNode = self:GetFrameRef("UIParent")
-		]])
+	UIHandle:SetFrameRef("UIParent", UIParent)
+	UIHandle:Execute([[
+		CurrentNode = self:GetFrameRef("UIParent")
+	]])
 
-		local ToggleCursor = CreateFrame("Button", addOn.."RaidCursorToggle", nil, "SecureActionButtonTemplate, SecureHandlerBaseTemplate")
-		ToggleCursor:RegisterForClicks("LeftButtonDown")
-		ToggleCursor:SetFrameRef("UIHandle", UIHandle)
-		ToggleCursor:SetAttribute("type", "target")
-		ToggleCursor:Execute([[
-			IsEnabled = false
-		]])
-		UIHandle:WrapScript(ToggleCursor, "OnClick", [[
-			IsEnabled = not IsEnabled
+	local ToggleCursor = CreateFrame("Button", addOn.."RaidCursorToggle", nil, "SecureActionButtonTemplate, SecureHandlerBaseTemplate")
+	ToggleCursor:RegisterForClicks("LeftButtonDown")
+	ToggleCursor:SetFrameRef("UIHandle", UIHandle)
+	ToggleCursor:SetAttribute("type", "target")
+	ToggleCursor:Execute([[
+		IsEnabled = false
+	]])
+	UIHandle:WrapScript(ToggleCursor, "OnClick", [[
+		IsEnabled = not IsEnabled
+		local UIHandle = self:GetFrameRef("UIHandle")
+		UIHandle:Run(ToggleCursor, IsEnabled)
+		if IsEnabled then
+			self:SetAttribute("unit", UIHandle:GetAttribute("node") and UIHandle:GetAttribute("node"):GetAttribute("unit"))
+		else
+			self:SetAttribute("unit", nil)
+		end
+	]])
+
+	local buttons = {
+		Up 		= {binding = "CP_L_UP", 	key = Key.Up},
+		Down 	= {binding = "CP_L_DOWN", 	key = Key.Down},
+		Left 	= {binding = "CP_L_LEFT", 	key = Key.Left},
+		Right 	= {binding = "CP_L_RIGHT",	key = Key.Right},
+	}
+
+	for name, button in pairs(buttons) do
+		local btn = CreateFrame("Button", addOn.."RaidCursorButton"..name, nil, "SecureActionButtonTemplate, SecureHandlerBaseTemplate")
+		btn:RegisterForClicks("LeftButtonDown", "LeftButtonUp")
+		btn:SetAttribute("type", "target")
+		btn:SetFrameRef("UIHandle", UIHandle)
+		UIHandle:WrapScript(btn, "OnClick", format([[
 			local UIHandle = self:GetFrameRef("UIHandle")
-			UIHandle:Run(ToggleCursor, IsEnabled)
-			if IsEnabled then
+			if down then
+				UIHandle:Run(SelectNode, %s)
 				self:SetAttribute("unit", UIHandle:GetAttribute("node") and UIHandle:GetAttribute("node"):GetAttribute("unit"))
 			else
 				self:SetAttribute("unit", nil)
 			end
-		]])
-
-		local buttons = {
-			Up 		= {binding = "CP_L_UP", 	key = Key.Up},
-			Down 	= {binding = "CP_L_DOWN", 	key = Key.Down},
-			Left 	= {binding = "CP_L_LEFT", 	key = Key.Left},
-			Right 	= {binding = "CP_L_RIGHT",	key = Key.Right},
-		}
-
-		for name, button in pairs(buttons) do
-			local btn = CreateFrame("Button", addOn.."RaidCursorButton"..name, nil, "SecureActionButtonTemplate, SecureHandlerBaseTemplate")
-			btn:RegisterForClicks("LeftButtonDown", "LeftButtonUp")
-			btn:SetAttribute("type", "target")
-			btn:SetFrameRef("UIHandle", UIHandle)
-			UIHandle:WrapScript(btn, "OnClick", format([[
-				local UIHandle = self:GetFrameRef("UIHandle")
-				if down then
-					UIHandle:Run(SelectNode, %s)
-					self:SetAttribute("unit", UIHandle:GetAttribute("node") and UIHandle:GetAttribute("node"):GetAttribute("unit"))
-				else
-					self:SetAttribute("unit", nil)
-				end
-			]], button.key))
-			UIHandle:Execute(format([[
-				Bindings.%s = "%s"
-			]], button.binding, name))
-		end
-
+		]], button.key))
+		UIHandle:Execute(format([[
+			Bindings.%s = "%s"
+		]], button.binding, name))
 	end
 	self.CreateRaidCursor = nil
 end
