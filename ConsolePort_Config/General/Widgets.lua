1d819921d4df43bd0c3ab8524b1c38f30fe8749e
Sebastian Lindfors <sl92cb@gmail.com>
2020-10-23 01:16:16 +0200

    string handling widget

--- ConsolePort_Config/General/Widgets.lua
+++ ConsolePort_Config/General/Widgets.lua
@@ -9,6 +9,23 @@ local COLOR_HILITE  = CPIndexButtonMixin.IndexColors.Hilite;
 local COLOR_NORMAL  = CPIndexButtonMixin.IndexColors.Normal;
 
 ---------------------------------------------------------------
+-- Create new widget
+---------------------------------------------------------------
+local function CreateWidget(name, inherit, blueprint)
+	local widget = CreateFromMixins(inherit)
+	widget.blueprint = blueprint;
+
+	Widgets[name] = function(self, varID, data)
+		db.table.mixin(self, widget)
+		self:OnLoad(varID, data)
+	end;
+
+	return widget;
+end
+
+Widgets.CreateWidget = CreateWidget;
+
+---------------------------------------------------------------
 -- Base widget object
 ---------------------------------------------------------------
 local Widget = {};
@@ -20,6 +37,10 @@ function Widget:OnLoad(varID, data)
 	self.tooltipText = data.desc;
 	self.controller:SetCallback(function(value) db('Settings/'..varID, value) end)
 	db:RegisterCallback('Settings/'..varID, self.OnValueChanged, self)
+
+	if self.blueprint then
+		Carpenter:BuildFrame(self, self.blueprint, false, true)
+	end
 end
 
 function Widget:Get()
@@ -58,6 +79,11 @@ function Widget:OnShow()
 	self:OnValueChanged(self:Get())
 end
 
+function Widget:OnClick()
+	self:SetChecked(false)
+	self:OnChecked(false)
+end
+
 function Widget:OnValueChanged(newValue)
 	-- replace callback in mixin
 end
@@ -65,42 +91,37 @@ end
 ---------------------------------------------------------------
 -- Boolean switch
 ---------------------------------------------------------------
-local Bool = CreateFromMixins(Widget);
-
-function Bool:OnLoad(varID, data)
-	Widget.OnLoad(self, varID, data)
-	Carpenter:BuildFrame(self, {
-		CheckBorder = {
-			_Type  = 'Texture';
-			_Setup = {'BACKGROUND'};
-			_Point = {'RIGHT', -8, 0};
-			_Size  = {24, 24};
-		};
-		CheckFill = {
-			_Type  = 'Texture';
-			_Setup = {'BORDER'};
-			_Point = {'CENTER', '$parent.CheckBorder', 'CENTER', 0, 0};
-			_Size  = {20, 20};
-			_SetColorTexture = {0, 0, 0, 1};
-		};
-		CheckedTexture = {
-			_Type  = 'Texture';
-			_Hide  = true;
-			_Setup = {'ARTWORK'};
-			_Point = {'CENTER', '$parent.CheckBorder', 'CENTER', 0, 0};
-			_Size  = {12, 12};
-			_SetColorTexture = {COLOR_CHECKED:GetRGBA()};
-		};
-		CheckedHilite = {
-			_Type  = 'Texture';
-			_Setup = {'HIGHLIGHT'};
-			_Point = {'CENTER', '$parent.CheckBorder', 'CENTER', 0, 0};
-			_Size  = {12, 12};
-			_Texture = CPAPI.GetAsset('Textures\\Frame\\Backdrop_Vertex_White');
-			_Vertex = {COLOR_HILITE:GetRGBA()};
-		};
-	}, false, true)
-end
+local Bool = CreateWidget('Bool', Widget, {
+	CheckBorder = {
+		_Type  = 'Texture';
+		_Setup = {'BACKGROUND'};
+		_Point = {'RIGHT', -8, 0};
+		_Size  = {24, 24};
+	};
+	CheckFill = {
+		_Type  = 'Texture';
+		_Setup = {'BORDER'};
+		_Point = {'CENTER', '$parent.CheckBorder', 'CENTER', 0, 0};
+		_Size  = {20, 20};
+		_SetColorTexture = {0, 0, 0, 1};
+	};
+	CheckedTexture = {
+		_Type  = 'Texture';
+		_Hide  = true;
+		_Setup = {'ARTWORK'};
+		_Point = {'CENTER', '$parent.CheckBorder', 'CENTER', 0, 0};
+		_Size  = {12, 12};
+		_SetColorTexture = {COLOR_CHECKED:GetRGBA()};
+	};
+	CheckedHilite = {
+		_Type  = 'Texture';
+		_Setup = {'HIGHLIGHT'};
+		_Point = {'CENTER', '$parent.CheckBorder', 'CENTER', 0, 0};
+		_Size  = {12, 12};
+		_Texture = CPAPI.GetAsset('Textures\\Frame\\Backdrop_Vertex_White');
+		_Vertex = {COLOR_HILITE:GetRGBA()};
+	};
+})
 
 function Bool:OnValueChanged(state)
 	self.CheckedTexture:SetShown(state)
@@ -113,55 +134,41 @@ function Bool:OnValueChanged(state)
 end
 
 function Bool:OnClick()
-	self:SetChecked(false)
-	self:OnChecked(false)
+	Widget.OnClick(self)
 	self:Set(not self:Get())
-end
-
-function Widgets.Bool(self, varID, data)
-	db.table.mixin(self, Bool)
-	self:OnLoad(varID, data)
 end
 
 ---------------------------------------------------------------
 -- Number
 ---------------------------------------------------------------
-local Number = CreateFromMixins(Widget);
-
-function Number:OnLoad(varID, data)
-	Widget.OnLoad(self, varID, data)
-	Carpenter:BuildFrame(self, {
-		Input = {
-			_Type  = 'EditBox';
-			_Setup = 'BackdropTemplate';
-			_Point = {'RIGHT', -8, 0};
-			_Size  = {60, 26};
-			_IgnoreNode = true;
-			_SetAutoFocus = false;
-			_SetFontObject = ChatFontNormal;
-			_SetTextInsets = {8, 8, 0, 0};
-			_Backdrop = CPAPI.Backdrops.Opaque;
-			_OnLoad = function(self)
-				self:SetBackdropColor(COLOR_NORMAL:GetRGBA());
-				self:SetBackdropBorderColor(0.15, 0.15, 0.15, 1);
-			end;
-			_OnTextChanged = function(input, byInput)
-				if byInput then
-					local value = tonumber(input:GetText())
-					if not value or (not self:GetSigned() and value < 0) then
-						input:SetText(self:Get())
-					else
-						self:Set(value)
-					end
+local Number = CreateWidget('Number', Widget, {
+	Input = {
+		_Type  = 'EditBox';
+		_Setup = 'BackdropTemplate';
+		_Point = {'RIGHT', -8, 0};
+		_Size  = {60, 26};
+		_IgnoreNode = true;
+		_SetAutoFocus = false;
+		_SetFontObject = ChatFontNormal;
+		_SetTextInsets = {8, 8, 0, 0};
+		_Backdrop = CPAPI.Backdrops.Opaque;
+		_OnLoad = function(self)
+			self:SetBackdropColor(COLOR_NORMAL:GetRGBA());
+			self:SetBackdropBorderColor(0.15, 0.15, 0.15, 1);
+		end;
+		_OnTextChanged = function(self, byInput)
+			local widget = self:GetParent()
+			if byInput then
+				local value = tonumber(self:GetText())
+				if not value or (not widget:GetSigned() and value < 0) then
+					self:SetText(widget:Get())
+				else
+					widget:Set(value)
 				end
-			end;
-		};
-	})
-end
-
---function Number:Set(value)
---	Widget.Set(self, self:GetSigned() and value or abs(value))
---end
+			end
+		end;
+	};
+})
 
 function Number:GetSigned()
 	return self.controller:GetSigned()
@@ -200,35 +207,28 @@ function Number:OnValueChanged(value)
 	self.Input:SetText(value)
 end
 
-function Widgets.Number(self, varID, data)
-	db.table.mixin(self, Number)
-	self:OnLoad(varID, data)
-end
-
 ---------------------------------------------------------------
 -- Range
 ---------------------------------------------------------------
-local Range = CreateFromMixins(Number);
-
-function Range:OnLoad(varID, data)
-	Widget.OnLoad(self, varID, data)
-	Carpenter:BuildFrame(self, {
-		Input = {
-			_Type  = 'Slider';
-			_Setup = 'CPConfigSliderTemplate';
-			_Point = {'RIGHT', -44, -6};
-			_IgnoreNode = true;
-			_SetObeyStepOnDrag = true;
-			_SetValueStep = {self:GetStep()};
-			_SetMinMaxValues = {self.controller:GetMinMax()};
-			_OnValueChanged = function(input, value, byInput)
-				if byInput then
-					self:Set(value)
-				end
-			end;
-		};
-	})
-end
+local Range = CreateWidget('Range', Number, {
+	Input = {
+		_Type  = 'Slider';
+		_Setup = 'CPConfigSliderTemplate';
+		_Point = {'RIGHT', -44, -6};
+		_IgnoreNode = true;
+		_SetObeyStepOnDrag = true;
+		_OnLoad = function(self)
+			local widget = self:GetParent()
+			self:SetValueStep(widget:GetStep());
+			self:SetMinMaxValues(widget.controller:GetMinMax());
+		end;
+		_OnValueChanged = function(self, value, byInput)
+			if byInput then
+				self:GetParent():Set(value)
+			end
+		end;
+	};
+})
 
 function Range:OnValueChanged(value)
 	local step = self.controller:GetStep()
@@ -236,35 +236,26 @@ function Range:OnValueChanged(value)
 	self.Input.Text:SetText(Round(value / step) * step)
 end
 
-function Widgets.Range(self, varID, data)
-	db.table.mixin(self, Range)
-	self:OnLoad(varID, data)
-end
-
 ---------------------------------------------------------------
 -- Delta
 ---------------------------------------------------------------
-local Delta = CreateFromMixins(Range);
-
-function Delta:OnLoad(varID, data)
-	Widget.OnLoad(self, varID, data)
-	Carpenter:BuildFrame(self, {
-		Input = {
-			_Type  = 'Slider';
-			_Setup = 'CPConfigSliderTemplate';
-			_Point = {'RIGHT', -44, -6};
-			_IgnoreNode = true;
-			_SetObeyStepOnDrag = true;
-			_SetValueStep = 2;
-			_SetMinMaxValues = {-1, 1};
-			_OnValueChanged = function(input, value, byInput)
-				if byInput and self.controller:IsOption(value) then
-					self:Set(value)
-				end
-			end;
-		};
-	})
-end
+local Delta = CreateWidget('Delta', Range, {
+	Input = {
+		_Type  = 'Slider';
+		_Setup = 'CPConfigSliderTemplate';
+		_Point = {'RIGHT', -44, -6};
+		_IgnoreNode = true;
+		_SetObeyStepOnDrag = true;
+		_SetValueStep = 2;
+		_SetMinMaxValues = {-1, 1};
+		_OnValueChanged = function(self, value, byInput)
+			local widget = self:GetParent()
+			if byInput and widget.controller:IsOption(value) then
+				widget:Set(value)
+			end
+		end;
+	};
+})
 
 function Delta:GetStep()
 	return 2;
@@ -275,7 +266,55 @@ function Delta:OnValueChanged(value)
 	self.Input.Text:SetText(value == 1 and '+' or value == -1 and '-')
 end
 
-function Widgets.Delta(self, varID, data)
-	db.table.mixin(self, Delta)
-	self:OnLoad(varID, data)
+---------------------------------------------------------------
+-- String
+---------------------------------------------------------------
+local String = CreateWidget('String', Widget, {
+	Input = {
+		_Type  = 'EditBox';
+		_Setup = 'BackdropTemplate';
+		_Point = {'RIGHT', -8, 0};
+		_Size  = {200, 26};
+		_IgnoreNode = true;
+		_SetAutoFocus = false;
+		_SetFontObject = ChatFontNormal;
+		_SetTextInsets = {8, 8, 0, 0};
+		_Backdrop = CPAPI.Backdrops.Opaque;
+		_OnLoad = function(self)
+			self:SetBackdropColor(COLOR_NORMAL:GetRGBA());
+			self:SetBackdropBorderColor(0.15, 0.15, 0.15, 1);
+		end;
+		_OnEscapePressed = function(self)
+			self:SetText(self:GetParent():Get() or '')
+			self:ClearFocus()
+		end;
+		_OnEnterPressed = function(self)
+			local text = self:GetText()
+			self:GetParent():Set(text:len() > 0 and text or nil)
+			self:ClearFocus()
+		end;
+		_OnEditFocusLost = function(self)
+			local widget = self:GetParent()
+			widget:SetChecked(false)
+			widget:OnChecked(false)
+		end;
+		_OnEditFocusGained = function(self)
+			local widget = self:GetParent()
+			widget:SetChecked(true)
+			widget:OnChecked(true)
+		end;
+	};
+})
+
+function String:OnClick()
+	local input = self.Input;
+	if input:HasFocus() then
+		self.Input:ClearFocus()
+	else
+		self.Input:SetFocus()
+	end
+end
+
+function String:OnValueChanged(value)
+	self.Input:SetText(value or '')
 end
