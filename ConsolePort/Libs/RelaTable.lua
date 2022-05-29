3e725f796d6f7a515a3abbcd7469f9e98a14e82a
Sebastian Lindfors <sl92cb@gmail.com>
2022-05-29 22:50:15 +0200

    Replace database handling

---
+++ ConsolePort/Libs/RelaTable.lua
@@ -0,0 +1,291 @@
+----------------------------------------------------------------
+-- RelaTable (╯°□°）╯︵ ┻━┻
+----------------------------------------------------------------
+-- 
+-- Author:  Sebastian Lindfors (Munk / MunkDev)
+-- Website: https://github.com/seblindfors/RelaTable
+-- Licence: GPL version 2 (General Public License)
+
+local Lib = LibStub:NewLibrary('RelaTable', 1)
+if not Lib then return end
+
+local Database = {};
+local compare, copy, map, merge, spairs, unravel
+local assert, strsplit, select, rawget, rawset, type = assert, strsplit, select, rawget, rawset, type
+
+----------------------------------------------------------------
+-- Database handler
+----------------------------------------------------------------
+local __mt = {
+    __call = function(self, ...)
+        local func = select('#', ...) > 1 and self.Set or self.Get
+        return func(self, ...)
+    end;
+}
+
+local function __cd(dir, idx, nxt, ...)
+    if not nxt then
+        return dir, tonumber(idx) or idx;
+    end
+    dir = dir[tonumber(idx) or idx];
+    if (dir == nil) then return end
+    return __cd(dir, nxt, ...)
+end
+
+function Database:Set(path, value)
+    local repo, var = __cd(self, strsplit('/', path))
+    if repo and var then
+        repo[var] = value;
+        self:TriggerEvent(path, value)
+        return true 
+    end
+end
+
+function Database:Get(path)
+    local repo, var = __cd(self, strsplit('/', path))
+    if repo and var then
+        local value = repo[var];
+        if (value == nil) then
+            return self:GetDefault(var)
+        end
+        return value;
+    end
+end
+
+function Database:GetDefault(var)
+    return self.default[var];
+end
+
+function Database:Save(path, ref, raw)
+    _G[ref] = raw and rawget(self, path) or self:Get(path)
+end
+
+function Database:Load(path, src, raw)
+    if raw then
+        return rawset(self, path, _G[src])
+    end
+    return self:Set(path, _G[src])
+end
+
+function Database:Register(name, obj, raw)
+    if not raw then
+        assert(not rawget(self, name), 'Object already exists.')
+    end
+    rawset(self, name, obj)
+    return obj;
+end
+
+function Database:Default(tbl)
+    rawset(self, 'default', tbl) -- add vars
+    return tbl;
+end
+
+function Database:Call(path, ...)
+    local repo, func = __cd(self, _G[_], path)
+    if repo and func then
+        return func(repo, ...)
+    end
+end
+
+function Database:Copy(path)
+    return copy(self:Get(path))
+end
+
+function Database:For(path, alphabetical)
+    local repo = self:Get(path)
+    assert(type(repo) == 'table', 'Path to database entry is invalid: ' .. tostring(path))
+    if #repo > 0 then
+        return ipairs(repo)
+    elseif alphabetical then
+        return spairs(repo)
+    end
+    return pairs(repo)
+end
+
+-- Callback handling proxy
+function Database:RegisterSafeCallback(...)
+    return self.callbacks:RegisterSafeCallback(...)
+end
+
+function Database:RegisterCallback(...)
+    return self.callbacks:RegisterCallback(...)
+end
+
+function Database:RegisterCallbacks(...)
+    local callback, owner = ...;
+    for i = 3, select('#', ...) do
+        self.callbacks:RegisterCallback(select(i, ...), callback, owner)
+    end
+end
+
+function Database:TriggerEvent(...)
+    return self.callbacks:TriggerEvent(...)
+end
+
+function Database:RunSafe(...)
+    return self.callbacks.safeCallback(...)
+end
+
+----------------------------------------------------------------
+-- Callback handler
+----------------------------------------------------------------
+local Callbacks = CreateFromMixins(CallbackRegistryMixin)
+
+function Callbacks:OnLoad()
+    CallbackRegistryMixin.OnLoad(self)
+    self:SetUndefinedEventsAllowed(true)
+    self:RegisterEvent('PLAYER_REGEN_ENABLED')
+    self:SetScript('OnEvent', self.OnSafeMode)
+    -- Create closure environment for safe callbacks
+    self.runSafeClosure  = function(f, args) f(unpack(args)) end;
+    self.runSafeClosures = {};
+    self.safeCallback    = function(func, ...)
+        if InCombatLockdown() then
+            return self:QueueSafeClosure(func, ...)
+        end
+        return func(...)
+    end;
+end
+
+function Callbacks:OnSafeMode()
+    foreach(self.runSafeClosures, self.runSafeClosure)
+    wipe(self.runSafeClosures)
+end
+
+function Callbacks:QueueSafeClosure(func, ...)
+    self.runSafeClosures[func] = {...};
+end
+
+function Callbacks:RegisterSafeCallback(event, ...)
+    return self:RegisterCallback(event, self.safeCallback, ...)
+end
+
+----------------------------------------------------------------
+-- Table utilities
+----------------------------------------------------------------
+function compare(t1, t2)
+    if t1 == t2 then
+        return true
+    elseif (t1 and not t2) or (t2 and not t1) then
+        return false
+    end
+    if type(t1) ~= "table" then
+        return false
+    end
+    local mt1, mt2 = getmetatable(t1), getmetatable(t2)
+    if not compare(mt1,mt2) then
+        return false
+    end
+    for k1, v1 in pairs(t1) do
+        local v2 = t2[k1]
+        if not compare(v1,v2) then
+            return false
+        end
+    end
+    for k2, v2 in pairs(t2) do
+        local v1 = t1[k2]
+        if not compare(v1,v2) then
+            return false
+        end
+    end
+    return true
+end
+
+function copy(src)
+    local srcType, t = type(src)
+    if srcType == "table" then
+        t = {}
+        for key, value in next, src, nil do
+            t[copy(key)] = copy(value)
+        end
+        setmetatable(t, copy(getmetatable(src)))
+    else
+        t = src
+    end
+    return t
+end
+
+function map(f, v, ...)
+    if (v ~= nil) then
+        return f(v), map(...)
+    end
+end
+
+function merge(t1, t2)
+    for k, v in pairs(t2) do
+        if (type(v) == "table") and (type(t1[k] or false) == "table") then
+            merge(t1[k], t2[k])
+        else
+            t1[k] = v
+        end
+    end
+    return t1
+end
+
+function spairs(t, order)
+    local keys = {unravel(t)}
+    if order then
+        table.sort(keys, function(a,b) return order(t, a, b) end)
+    else
+        table.sort(keys)
+    end
+    local i, k = 0
+    return function()
+        i = i + 1
+        k = keys[i]
+        if k then
+            return k, t[k]
+        end
+    end
+end
+
+function unravel(t, i)
+    local k = next(t, i)
+    if k ~= nil then
+        return k, unravel(t, k)
+    end
+end
+
+local TableUtils = setmetatable({
+    compare = compare;
+    copy    = copy;
+    map     = map;
+    merge   = merge;
+    spairs  = spairs;
+    unravel = unravel;
+}, { __index = table })
+
+
+----------------------------------------------------------------
+-- Lib
+----------------------------------------------------------------
+setmetatable(Lib, {
+    __newindex = nop;
+    __call = function(self, db, id, silent)
+        if id then
+            if rawget(self, id) ~= nil then
+                if silent then
+                    return
+                end
+                error(('Database %s already exists.'):format(tostring(id)))
+            end
+            rawset(self, id, db)
+        end
+
+        local callbackHandle = Mixin(CreateFrame('Frame'), Callbacks)
+        callbackHandle:OnLoad()
+
+        Mixin(db, Database)
+        db.table = copy(TableUtils);
+        db.default = db;
+        db.callbacks = callbackHandle;
+
+        if (EventRegistry and EventRegistry.TriggerEvent) then
+            hooksecurefunc(EventRegistry, 'TriggerEvent', function(_, ...)
+                db:TriggerEvent(...)
+            end)
+        end
+
+        return setmetatable(db, __mt), callbackHandle;
+    end;
+})
--- ConsolePort/Utils/Database.lua
+++ ConsolePort/Utils/Database.lua
@@ -1,100 +1,68 @@
-local strsplit, select, _, db = strsplit, select, ...
------------------------------------------------------------
--- DB Usage:
---  @set      db('[pathto/]var', value)
---  @get      db('[pathto/]var')
--- Registry:
---  @save     db:Save('[pathto/]var', 'asGlobalName', raw)
---  @load     db:Load('[pathto/]var', 'globalName', raw)
---  @default  db:Default(tbl)
---  @register db:Register('name', obj)
------------------------------------------------------------
-db.default = db -- fallback
+local _, db = ...
+---------------------------------------------------------------
+-- Initialize database
+---------------------------------------------------------------
+LibStub('RelaTable')(db, _)
 
-local function __cd(dir, idx, nxt, ...)
-	if not nxt then
-		return dir, tonumber(idx) or idx
+-- Provide a wrapper for default field data
+function db:GetDefault(var)
+	local varDefault = self.default[var]
+	if (type(varDefault) == 'table' and varDefault.Get) then
+		return varDefault:Get()
 	end
-	dir = dir[tonumber(idx) or idx]
-	if (dir == nil) then return end
-	return __cd(dir, nxt, ...)
+	return varDefault
 end
 
-function db:Set(path, value)
-	local repo, var = __cd(self, strsplit('/', path))
-	if repo and var then
-		repo[var] = value
-		self:TriggerEvent(path, value)
-		return true 
-	end
-end
+---------------------------------------------------------------
+-- Extra table functions for various uses
+---------------------------------------------------------------
+-- This is a special iterator that returns a table in reverse order,
+-- or by "intuitive" modifier order. E.g. Shift comes before Ctrl.
+local mIndex = setmetatable({
+	['']                = 1;
+	['SHIFT-']          = 2;
+	['CTRL-']           = 3;
+	['ALT-']            = 4;
+	['CTRL-SHIFT-']     = 5;
+	['ALT-SHIFT-']      = 6;
+	['ALT-CTRL-']       = 7;
+	['ALT-CTRL-SHIFT-'] = 8;
+}, {__index = function() return 9 end})
 
-function db:Get(path)
-	local repo, var = __cd(self, strsplit('/', path))
-	if repo and var then
-		local value = repo[var]
-		if (value == nil) then
-			local varDefault = self.default[var]
-			if (type(varDefault) == 'table' and varDefault.Get) then
-				return varDefault:Get()
-			end
-			return varDefault
+db('table/mpairs', function(t)
+	return db.table.spairs(t, function(t, a, b)
+		return mIndex[a] < mIndex[b];
+	end)
+end)
+
+db('table/ripairs', ripairs or function(t)
+	local function ripairsiter(t, index)
+		index = index - 1;
+		if index > 0 then
+			return index, t[index];
 		end
-		return value
 	end
-end
+	return ripairsiter, t, #t + 1;
+end)
 
-function db:Save(path, ref, raw)
-	_G[ref] = raw and rawget(self, path) or self:Get(path)
-end
+db('table/mixin', function(obj, ...)
+	local scriptHandler = (type(obj.HasScript) == 'function')
+	for i = 1, select('#', ...) do
+		local mixin = select(i, ...)
 
-function db:Load(path, src, raw)
-	if raw then
-		return rawset(self, path, _G[src])
+		for k, v in pairs(mixin) do
+			if scriptHandler and obj:HasScript(k) then
+				if obj:GetScript(k) then
+					obj:HookScript(k, v)
+				else
+					obj:SetScript(k, v)
+				end
+			end
+			obj[k] = v
+		end
 	end
-	return self:Set(path, _G[src])
-end
-
-function db:Register(name, obj, raw)
-	if not raw then
-		assert(not rawget(self, name), 'Object already exists.')
-	end
-	rawset(self, name, obj)
 	return obj
-end
-
-function db:Default(tbl)
-	rawset(self, 'default', tbl) -- add vars
-	return tbl
-end
-
-function db:Call(path, ...)
-	local repo, func = __cd(self, _G[_], path)
-	if repo and func then
-		return func(repo, ...)
-	end
-end
-
-function db:For(path, alphabetical)
-	local repo = self:Get(path)
-	assert(type(repo) == 'table', 'Path to database entry is invalid: ' .. tostring(path))
-	if #repo > 0 then
-		return ipairs(repo)
-	elseif alphabetical then
-		return self.table.spairs(repo)
-	end
-	return pairs(repo)
-end
-
-setmetatable(db, {
-	__call = function(self, ...)
-		local func = select('#', ...) > 1 and self.Set or self.Get
-		return func(self, ...)
-	end;
-	__newindex = function()
-		error('Access denied. Use ConsolePort:DB(...) to set/get data.')
-	end;
-})
+end)
 
 ---------------------------------------------------------------
 -- Plug-in access to addon data
