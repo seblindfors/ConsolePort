98fe528bed3d6c1a0c4a1e4d1a7a5982de5a04b4
Sebastian Lindfors <sl92cb@gmail.com>
2023-03-02 01:45:45 +0100

    Refactor and fix graphics

--- ConsolePort/View/PowerLevel/PowerLevel.lua
+++ ConsolePort/View/PowerLevel/PowerLevel.lua
@@ -9,20 +9,21 @@ local FadeIn, FadeOut = db.Alpha.FadeIn, db.Alpha.FadeOut;
 
 local FADE_SPEED = 0.25;
 
-PowerLevel.Levels = {
-	{fill = 1, color = RED_FONT_COLOR,    name = L'Critical', animation = 'Critical'};
-	{fill = 1, color = ORANGE_FONT_COLOR, name = L'Low'};
-	{fill = 2, color = YELLOW_FONT_COLOR, name = L'Medium'};
-	{fill = 3, color = GREEN_FONT_COLOR,  name = L'High'};
-	{fill = 3, color = BLUE_FONT_COLOR,   name = L'Charging', animation = 'Charging'};
-	{fill = 0, color = WHITE_FONT_COLOR,  name = L'Disconnected'};
-}
+PowerLevel.Levels = CPAPI.Proxy({
+	{fill = 1, color = RED_FONT_COLOR,    atlas = 'ui-frame-bar-fill-red',    name = L'Critical', animation = 'Critical'};
+	{fill = 1, color = ORANGE_FONT_COLOR, atlas = 'ui-frame-bar-fill-yellow', name = L'Low'};
+	{fill = 2, color = YELLOW_FONT_COLOR, atlas = 'ui-frame-bar-fill-yellow', name = L'Medium'};
+	{fill = 3, color = GREEN_FONT_COLOR,  atlas = 'ui-frame-bar-fill-green',  name = L'High'};
+	{fill = 3, color = BLUE_FONT_COLOR,   atlas = 'ui-frame-bar-fill-blue',   name = L'Charging', animation = 'Charging'};
+	{fill = 0, color = WHITE_FONT_COLOR,  atlas = 'ui-frame-bar-fill-white',  name = L'Disconnected'};
+}, function() return
+	{fill = 3, color = WHITE_FONT_COLOR,  atlas = 'ui-frame-bar-fill-white',  name = UNKNOWN}
+end)
 
 function PowerLevel:SetPowerLevel(level)
 	local info = self.Levels[level + 1];
-	local text = self.OverlayFrame.Text;
 
-	FadeOut(text, FADE_SPEED, text:GetAlpha(), 0)
+	FadeOut(self.Text, FADE_SPEED, self.Text:GetAlpha(), 0)
 
 	if self.currentAnimation then
 		self.currentAnimation:Stop()
@@ -30,9 +31,9 @@ function PowerLevel:SetPowerLevel(level)
 		self.currentAnimation = nil;
 	end
 
-	text:SetFormattedText(info.name)
+	self.Text:SetFormattedText(info.name)
 
-	FadeIn(text, FADE_SPEED, 0, 1)
+	FadeIn(self.Text, FADE_SPEED, 0, 1)
 	self.currentAnimation = info.animation and self[info.animation];
 	if self.currentAnimation then
 		self.currentAnimation:Play()
@@ -40,7 +41,11 @@ function PowerLevel:SetPowerLevel(level)
 	end
 
 	self:SetValue(info.fill)
-	self.BarTexture:SetVertexColor(info.color:GetRGB())
+	if C_Texture.GetAtlasInfo(info.atlas) then
+		self.BarTexture:SetAtlas(info.atlas)
+	else
+		self.BarTexture:SetVertexColor(info.color:GetRGB())
+	end
 	FadeIn(self, 0, 1)
 end
 
@@ -51,21 +56,18 @@ function PowerLevel:OnDataLoaded()
 	local showIcon = db('powerLevelShowIcon')
 	local showText = db('powerLevelShowText')
 
-	local text = self.OverlayFrame.Text;
-	local icon = self.OverlayFrame.Icon;
-
 	self:SetPowerLevel(db.Gamepad:GetPowerLevel())
 	FadeIn(self, FADE_SPEED, self:GetAlpha(), 1)
 	if showText then
-		FadeIn(text, FADE_SPEED, text:GetAlpha(), 1)
+		FadeIn(self.Text, FADE_SPEED, self.Text:GetAlpha(), 1)
 	else
-		FadeOut(text, FADE_SPEED, text:GetAlpha(), 0)
+		FadeOut(self.Text, FADE_SPEED, self.Text:GetAlpha(), 0)
 	end
 	if showIcon then
-		db.Gamepad.SetIconToTexture(icon, 'PADSYSTEM')
-		FadeIn(icon, FADE_SPEED, icon:GetAlpha(), 1)
+		db.Gamepad.SetIconToTexture(self.Icon, 'PADSYSTEM')
+		FadeIn(self.Icon, FADE_SPEED, self.Icon:GetAlpha(), 1)
 	else
-		FadeOut(icon, FADE_SPEED, icon:GetAlpha(), 0)
+		FadeOut(self.Icon, FADE_SPEED, self.Icon:GetAlpha(), 0)
 	end
 
 	if CPAPI.IsRetailVersion then
@@ -81,7 +83,6 @@ db:RegisterCallbacks(PowerLevel.OnDataLoaded, PowerLevel,
 	'Settings/powerLevelShowIcon',
 	'Settings/powerLevelShowText'
 )
-
 
 ---------------------------------------------------------------
 -- Scripts
@@ -112,3 +113,26 @@ end
 
 CPAPI.Start(PowerLevel)
 CPAPI.EventHandler(PowerLevel)
+
+---------------------------------------------------------------
+-- Textures
+---------------------------------------------------------------
+if CPAPI.IsRetailVersion then
+	PowerLevel.BorderLeft:SetAtlas('ui-frame-bar-borderleft')
+	PowerLevel.BorderRight:SetAtlas('ui-frame-bar-borderright')
+	PowerLevel.BorderCenter:SetAtlas('ui-frame-bar-bordercenter')
+	PowerLevel.Tick1:SetAtlas('ui-frame-bar-bordertick')
+	PowerLevel.Tick2:SetAtlas('ui-frame-bar-bordertick')
+	PowerLevel.GlowLeft:SetAtlas('ui-frame-bar-glowleft')
+	PowerLevel.GlowRight:SetAtlas('ui-frame-bar-glowright')
+	PowerLevel.GlowCenter:SetAtlas('ui-frame-bar-glowcenter')
+else
+	PowerLevel.BorderCenter:SetAtlas('bonusobjectives-bar-frame')
+	PowerLevel.BorderCenter:ClearAllPoints()
+	PowerLevel.BorderCenter:SetSize(130, 34)
+	PowerLevel.BorderCenter:SetPoint('CENTER', 10, 0)
+	PowerLevel.GlowCenter:SetAtlas('bonusobjectives-bar-glow')
+	PowerLevel.GlowCenter:ClearAllPoints()
+	PowerLevel.GlowCenter:SetSize(114, 24)
+	PowerLevel.GlowCenter:SetPoint('CENTER', 1, 0)
+end
