276e1f44e1c297d5dd4f23b474d7c74a548162eb
Sebastian Lindfors <sl92cb@gmail.com>
2023-11-24 12:08:11 +0100

    Add custom cursor scripts for Retail talent frame

--- ConsolePort_Cursor/Controller/Scripts.lua
+++ ConsolePort_Cursor/Controller/Scripts.lua
@@ -6,7 +6,7 @@
 -- because they modify properties of protected objects, either
 -- directly or indirectly by execution path.
 
-local _, env = ...;
+local _, env, L = ...; L = env.db.Locale;
 local Execute, Scripts = ExecuteFrameScript, CPAPI.Proxy({}, function(self, key) return rawget(rawset(self, key, {}), key) end);
 
 function env.TriggerScript(node, scriptType, ...)
@@ -29,7 +29,6 @@ function env.ReplaceScript(scriptType, original, replacement)
 end
 
 function _(addOn, script) EventUtil.ContinueOnAddOnLoaded(addOn, GenerateClosure(pcall, script)) end
-
 ---------------------------------------------------------------
 -- Scripts: OnEnter
 ---------------------------------------------------------------
@@ -73,7 +72,9 @@ do
 		end
 	end
 	if CPAPI.IsRetailVersion then
+	-----------------------------------------------------------
 		_('Blizzard_Collections', function()
+	-----------------------------------------------------------
 			Scripts.OnEnter[ ToySpellButton_OnEnter ] = function(self)
 				-- strips fanfare/UpdateTooltip from toy spell buttons, since the taint prevents UseToy from working
 				if self.itemID then
@@ -83,12 +84,72 @@ do
 				end
 			end
 		end)
+	-----------------------------------------------------------
 		_('Blizzard_ClassTalentUI', function()
-			-- also taints action bar controller through highlights
-			Scripts.OnEnter[ ClassTalentButtonSpendMixin.OnEnter ]       = TalentButtonSpendMixin.OnEnter;
-			Scripts.OnEnter[ ClassTalentButtonSelectMixin.OnEnter ]      = TalentButtonSelectMixin.OnEnter;
-			Scripts.OnEnter[ ClassTalentButtonSplitSelectMixin.OnEnter ] = TalentButtonSplitSelectMixin.OnEnter;
-			Scripts.OnEnter[ ClassTalentSelectionChoiceMixin.OnEnter ]   = TalentDisplayMixin.OnEnter;
+	-----------------------------------------------------------
+			-- Talent frame customization:
+			-- Remove action bar highlights from talent buttons, since they taint the action bar controller.
+			-- Also, add a special click handler to split talent buttons, so that they can be selected by clicking on them,
+			-- instead of on mouseover. Finally, hook the spell menu to remove focus from the talent frame so that
+			-- pickups and bar placements from the talent frame can go smoothly.
+
+			local selectionChoiceFrame = ClassTalentFrame.TalentsTab.SelectionChoiceFrame;
+			local currentBaseButton;
+
+			Scripts.OnEnter[ ClassTalentButtonSpendMixin.OnEnter ] = function(self)
+				selectionChoiceFrame:Hide()
+				TalentButtonSpendMixin.OnEnter(self)
+			end;
+			Scripts.OnEnter[ ClassTalentButtonSelectMixin.OnEnter ] = function(self)
+				selectionChoiceFrame:Hide()
+				TalentButtonSelectMixin.OnEnter(self)
+			end;
+			Scripts.OnEnter[ ClassTalentSelectionChoiceMixin.OnEnter ] = TalentDisplayMixin.OnEnter;
+			Scripts.OnEnter[ ClassTalentButtonSplitSelectMixin.OnEnter ] = function(self)
+				self:SetAttribute(env.Attributes.SpecialClick, function(self, button, down)
+					TalentButtonSplitSelectMixin.OnEnter(self)
+					CPAPI.NextRender(function()
+						env.Cursor:SetCurrentNode(selectionChoiceFrame.selectionFrameArray[1])
+					end)
+				end)
+
+				selectionChoiceFrame:Hide()
+				currentBaseButton = self;
+
+				GameTooltip:SetOwner(self, 'ANCHOR_RIGHT')
+				local selectTalentText = L'Select talent';
+				local spellID = self:GetSpellID()
+				if spellID then
+					GameTooltip:SetSpellByID(spellID)
+					GameTooltip:AddLine(env.Hooks:GetSpecialActionPrompt(selectTalentText))
+					GameTooltip:Show()
+				else
+					GameTooltip:SetText(env.Hooks:GetSpecialActionPrompt(selectTalentText))
+					GameTooltip:Show()
+				end
+			end;
+
+			selectionChoiceFrame:HookScript('OnHide', function(self)
+				if currentBaseButton then
+					CPAPI.NextRender(function()
+						env.Cursor:SetCurrentNode(currentBaseButton)
+						currentBaseButton = nil;
+					end)
+				end
+			end)
+
+			ConsolePortSpellMenu:HookScript('OnShow', function()
+				if ClassTalentFrame:IsShown() then
+					ClassTalentFrame:SetAlpha(0.25)
+					ClassTalentFrame:SetAttribute(env.Attributes.IgnoreNode, true)
+				end
+			end)
+			ConsolePortSpellMenu:HookScript('OnHide', function()
+				if ClassTalentFrame:GetAttribute(env.Attributes.IgnoreNode) then
+					ClassTalentFrame:SetAlpha(1)
+					ClassTalentFrame:SetAttribute(env.Attributes.IgnoreNode, nil)
+				end
+			end)
 		end)
 	end
 end
@@ -104,12 +165,36 @@ do
 		end
 	end
 	if CPAPI.IsRetailVersion then
+	-----------------------------------------------------------
 		_('Blizzard_ClassTalentUI', function()
-			-- also taints action bar controller through highlights
-			Scripts.OnLeave[ ClassTalentButtonSpendMixin.OnLeave ]       = TalentDisplayMixin.OnLeave;
-			Scripts.OnLeave[ ClassTalentButtonSelectMixin.OnLeave ]      = TalentButtonSelectMixin.OnLeave;
+	-----------------------------------------------------------
+			-- Talent frame customization:
+			-- Remove clearing of action bar highlights from talent buttons, since they taint the action bar controller.
+			-- When leaving a split choice talent popup, hide the popup if the cursor is not over a nested selection button.
+
+			local selectionChoiceFrame = ClassTalentFrame.TalentsTab.SelectionChoiceFrame;
+
+			Scripts.OnLeave[ ClassTalentButtonSpendMixin.OnLeave ] = function(self)
+				selectionChoiceFrame:Hide()
+				TalentDisplayMixin.OnLeave(self)
+			end;
+			Scripts.OnLeave[ ClassTalentButtonSelectMixin.OnLeave ] = function(self)
+				selectionChoiceFrame:Hide()
+				TalentButtonSelectMixin.OnLeave(self)
+			end;
+
 			Scripts.OnLeave[ ClassTalentButtonSplitSelectMixin.OnLeave ] = TalentButtonSplitSelectMixin.OnLeave;
-			Scripts.OnLeave[ ClassTalentSelectionChoiceMixin.OnLeave ]   = TalentDisplayMixin.OnLeave;
+			Scripts.OnLeave[ ClassTalentSelectionChoiceMixin.OnLeave ] = function(self)
+				TalentDisplayMixin.OnLeave(self)
+				CPAPI.NextRender(function()
+					if ConsolePortSpellMenu:IsShown() then return end;
+					
+					local currentNode = env.Cursor:GetCurrentNode()
+					if currentNode and currentNode:GetParent() ~= selectionChoiceFrame then
+						selectionChoiceFrame:Hide()
+					end
+				end)
+			end;
 		end)
 	end
 end
