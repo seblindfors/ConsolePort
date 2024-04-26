1900888eb1cc7784b3f67f598dd78e1c3d823421
Sebastian Lindfors <sl92cb@gmail.com>
2024-04-26 16:25:24 +0200

    Refactor action bar database utilities to separate file

--- ConsolePort_Bar/Database.lua
+++ ConsolePort_Bar/Database.lua
@@ -1,221 +1,4 @@
-local env = LibStub('RelaTable')(...)
-env.db        = ConsolePort:DB();
-env.UIHandler = CPAPI.CreateEventHandler({'Frame'}, {
-	-- events go here
-}); local UIHandler = env.UIHandler;
-
-UIHandler:Hide()
-function UIHandler:OnDataLoaded()
-	self:HideBlizzard()
-	env:TriggerEvent('OnEnvLoaded')
-end
-
----------------------------------------------------------------
--- Libs
----------------------------------------------------------------
-env.LIB = LibStub('ConsolePortActionButton')
-env.LAB = LibStub('LibActionButton-1.0')
-
----------------------------------------------------------------
--- Presets
----------------------------------------------------------------
-
-env.Presets = {};
-env.Presets.Default = function() return {
-	name = DEFAULT;
-	desc = 'A cluster bar with a toolbar below it.';
-	visibility = '[petbattle][vehicleui][overridebar] hide; show';
-	bars = {
-		Cluster = {
-			type     = 'Cluster';
-			lock     = true;
-			scale 	 = 1;
-			width 	 = 1200;
-			height   = 140;
-			point    = { point = 'BOTTOM', x = 0, y = 16};
-			buttons  = (function(buttons)
-				local handle = env.db.UIHandle;
-				local T1, T2 = handle:GetUIControlBinding('T1'), handle:GetUIControlBinding('T2')
-				local M1, M2 = handle:GetUIControlBinding('M1'), handle:GetUIControlBinding('M2')
-
-				if M1 then buttons[M1] = { dir = 'UP',    point = {'LEFT',   405, 75}, size = 64 } end;
-				if M2 then buttons[M2] = { dir = 'UP',    point = {'RIGHT', -405, 75}, size = 64 } end;
-				if T1 then buttons[T1] = { dir = 'RIGHT', point = {'LEFT',   440,  9}, size = 64 } end;
-				if T2 then buttons[T2] = { dir = 'LEFT',  point = {'RIGHT', -440,  9}, size = 64 } end;
-
-				return buttons;
-			end)({
-				PADDRIGHT = { dir = 'RIGHT', point = {'LEFT',   330, 9}, size = 64 };
-				PADDLEFT  = { dir = 'LEFT',  point = {'LEFT',    80, 9}, size = 64 };
-				PADDDOWN  = { dir = 'DOWN',  point = {'LEFT',   165, 9}, size = 64 };
-				PADDUP    = { dir = 'UP',    point = {'LEFT',   250, 9}, size = 64 };
-				PAD2      = { dir = 'RIGHT', point = {'RIGHT',  -80, 9}, size = 64 };
-				PAD3      = { dir = 'LEFT',  point = {'RIGHT', -330, 9}, size = 64 };
-				PAD1      = { dir = 'DOWN',  point = {'RIGHT', -250, 9}, size = 64 };
-				PAD4      = { dir = 'UP',    point = {'RIGHT', -165, 9}, size = 64 };
-			});
-		};
-		Toolbar = {
-			type    = 'Toolbar';
-			lock     = true;
-			scale    = 1;
-			width    = 900;
-			point    = { point = 'BOTTOM', x = 0, y = 0};
-		};
-	};
-}; end;
-
-
-function env:GetDefaultLayout()
-	return env.Presets.Default();
-end
-
----------------------------------------------------------------
-do -- Data handler
----------------------------------------------------------------
-local VAR_SETTINGS, VAR_LAYOUT = 'ConsolePort_BarDB', 'ConsolePort_BarLayout';
-
-function env:UpdateDataSource()
-	local settings, layout;
-	-- TODO: Potentially add global/character setting switch
-	if not _G[VAR_SETTINGS] then
-		_G[VAR_SETTINGS] = {};
-	end
-	if not _G[VAR_LAYOUT] then
-		_G[VAR_LAYOUT] = env:GetDefaultLayout()
-	end
-
-	layout = _G[VAR_LAYOUT];
-	settings = CPAPI.Proxy(_G[VAR_SETTINGS], self.Defaults);
-
-	env:Register('Layout', layout, true)
-	env:Register('Settings', settings, true)
-	env:Default(settings)
-	env:Save('Settings', VAR_SETTINGS)
-	env:Save('Layout', VAR_LAYOUT)
-end
-
-function env:GetDefault(var)
-	local varDefault = self.default[var];
-	if (type(varDefault) == 'table' and varDefault.Get) then
-		return varDefault:Get()
-	end
-	return varDefault;
-end
-
-function env:OnVariablesChanged(variables)
-	for varID, data in pairs(variables) do
-		self.Defaults[varID] = data[1]:Get();
-	end
-end
-
-function env:OnEnvLoaded()
-	self.Defaults = {};
-	self:OnVariablesChanged(env.Variables)
-	self:UpdateDataSource()
-	env:TriggerEvent('OnDataLoaded')
-end
-
-env:RegisterCallback('OnEnvLoaded', env.OnEnvLoaded, env)
-end -- Data handler
-
----------------------------------------------------------------
-do -- Widget factory
----------------------------------------------------------------
-env.Factories, env.Widgets, env.ActiveWidgets = {}, {}, {};
-
-local function HideAndClearAnchors(widget)
-	widget:Hide()
-	widget:ClearAllPoints()
-end
-
-function env:AddFactory(type, factory)
-	self.Factories[type] = factory;
-end
-
-function env:Acquire(frameType, id, ...)
-	assert(self.Factories[frameType], 'Factory does not exist: '..frameType)
-	assert(type(id) == 'string', 'Factory widget ID must be a string')
-	local signature = self.MakeSig(frameType, id);
-	if not self.Widgets[signature] then
-		self.Widgets[signature] = self.Factories[frameType](id, ...);
-	end
-	self.ActiveWidgets[self.Widgets[signature]] = signature;
-	return self.Widgets[signature];
-end
-
-function env:Map(frameType, id, func, ...)
-	local signature = '^'..self.MakeSig(frameType, id);
-	for widget, sig in pairs(self.ActiveWidgets) do
-		if sig:find(signature) then
-			func(widget, ...);
-		end
-	end
-end
-
-function env:GetSignature(widget)
-	return self.ActiveWidgets[widget];
-end
-
-function env:IsActive(widget)
-	return not not self.ActiveWidgets[widget];
-end
-
-function env:Release(widget, release)
-	self.ActiveWidgets[widget] = nil;
-	(release or HideAndClearAnchors)(widget);
-	if widget.OnRelease then
-		widget:OnRelease();
-	end
-end
-
-end -- Widget factory
-
-
----------------------------------------------------------------
--- Asset data handler
----------------------------------------------------------------
-env.Colors = {};
-
-function env:GetColor(id)
-	local hex = self(id)
-	local color = self.Colors[id];
-	if color and (hex == color.hex) then
-		return color;
-	end
-	color = CPAPI.CreateColorFromHexString(hex);
-	color.hex = hex;
-	self.Colors[id] = color;
-	return color;
-end
-
-function env:GetColorRGB(id)
-	local color = self:GetColor(id);
-	return color:GetRGB();
-end
-
-function env:GetColorGradient(r, g, b, a) a = a or 1;
-	local base, mult = 0.15, 1.2;
-	local startA, endA = 0.25 * a, 0;
-	return -- SetGradient
-	--[[ orientation ]] 'VERTICAL',
-	--[[ minColor    ]] CreateColor((r + base) * mult, (g + base) * mult, (b + base) * mult, startA),
-	--[[ maxColor    ]] CreateColor(1 - (r + base) * mult, 1 - (g + base) * mult, 1 - (b + base) * mult, endA);
-end
-
-function env.GetAsset(asset, arg1, ...)
-	return [[Interface\AddOns\ConsolePort_Bar\Assets\]]
-		..(arg1 and asset:format(arg1, ...) or asset);
-end
-
-function env.MakeID(name, ...)
-	return name:format(...):gsub('-', '_'):gsub('_$', '')
-end
-
-function env.MakeSig(type, id)
-	return type..':'..(id or '');
-end
-
+local _, env = ...;
 ---------------------------------------------------------------
 do -- Variables
 ---------------------------------------------------------------
@@ -263,9 +46,9 @@ end -- Variables
 
 
 ---------------------------------------------------------------
--- Cluster information
+do -- Cluster information
 ---------------------------------------------------------------
-do local M1, M2, M3 = 'M1', 'M2', 'M3';
+local M1, M2, M3 = 'M1', 'M2', 'M3';
 ---------------------------------------------------------------
 local NOMOD,  SHIFT,    CTRL,    ALT =
       '',    'SHIFT-', 'CTRL-', 'ALT-';
@@ -376,7 +159,7 @@ env.ClusterConstants = {
 	};
 	LABConfig = {
 		clickOnDown     = true;
-		flyoutDirection = 'UP';
+		flyoutDirection = 'RIGHT';
 		showGrid        = true;
 		tooltip         = 'enabled';
 		colors = {
@@ -391,7 +174,7 @@ env.ClusterConstants = {
 	};
 };
 
-env.ClusterConstants.Masks,env.ClusterConstants.Swipes = (function(layout, directions)
+env.ClusterConstants.Masks, env.ClusterConstants.Swipes = (function(layout, directions)
 	-- Generated masks and swipes for each prefix, e.g.:
 	-- env.ClusterConstants.Masks.M1.UP = 'MASKS\M1_UP'
 	local masks, swipes = {}, {};
@@ -413,8 +196,63 @@ env.ClusterConstants.ModDriver = (function(driver, ...)
 		-- Insert in reverse order to prioritize most complex modifiers
 		tinsert(driver, 1, ('[mod:%s] %s'):format(modifier, modifier))
 	end
-	driver[#driver] = ' '; -- NOMOD fix
-	return table.concat(driver, ';');
+	driver[#driver] = '[nomod]'; -- NOMOD fix
+	return table.concat(driver, '; ')..' ;';
 end)( {}, env.ClusterConstants.ModNames() )
 
 end -- Cluster information
+
+
+
+---------------------------------------------------------------
+-- Presets
+---------------------------------------------------------------
+env.Presets = {};
+env.Presets.Default = function() return {
+	name = DEFAULT;
+	desc = 'A cluster bar with a toolbar below it.';
+	visibility = '[petbattle][vehicleui][overridebar] hide; show';
+	bars = {
+		Cluster = {
+			type     = 'Cluster';
+			lock     = true;
+			scale 	 = 1;
+			width 	 = 1200;
+			height   = 140;
+			point    = { point = 'BOTTOM', x = 0, y = 16};
+			buttons  = (function(buttons)
+				local handle = env.db.UIHandle;
+				local T1, T2 = handle:GetUIControlBinding('T1'), handle:GetUIControlBinding('T2')
+				local M1, M2 = handle:GetUIControlBinding('M1'), handle:GetUIControlBinding('M2')
+
+				if M1 then buttons[M1] = { dir = 'UP',    point = {'LEFT',   405, 75}, size = 64 } end;
+				if M2 then buttons[M2] = { dir = 'UP',    point = {'RIGHT', -405, 75}, size = 64 } end;
+				if T1 then buttons[T1] = { dir = 'RIGHT', point = {'LEFT',   440,  9}, size = 64 } end;
+				if T2 then buttons[T2] = { dir = 'LEFT',  point = {'RIGHT', -440,  9}, size = 64 } end;
+
+				return buttons;
+			end)({
+				PADDRIGHT = { dir = 'RIGHT', point = {'LEFT',   330, 9}, size = 64 };
+				PADDLEFT  = { dir = 'LEFT',  point = {'LEFT',    80, 9}, size = 64 };
+				PADDDOWN  = { dir = 'DOWN',  point = {'LEFT',   165, 9}, size = 64 };
+				PADDUP    = { dir = 'UP',    point = {'LEFT',   250, 9}, size = 64 };
+				PAD2      = { dir = 'RIGHT', point = {'RIGHT',  -80, 9}, size = 64 };
+				PAD3      = { dir = 'LEFT',  point = {'RIGHT', -330, 9}, size = 64 };
+				PAD1      = { dir = 'DOWN',  point = {'RIGHT', -250, 9}, size = 64 };
+				PAD4      = { dir = 'UP',    point = {'RIGHT', -165, 9}, size = 64 };
+			});
+		};
+		Toolbar = {
+			type    = 'Toolbar';
+			lock     = true;
+			scale    = 1;
+			width    = 900;
+			point    = { point = 'BOTTOM', x = 0, y = 0};
+		};
+	};
+}; end;
+
+
+function env:GetDefaultLayout()
+	return env.Presets.Default();
+end
