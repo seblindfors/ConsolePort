52d469e1393097dd18e9987e64f8cc8db3bb293d
Sebastian Lindfors <sl92cb@gmail.com>
2024-05-08 08:10:41 +0200

    Lazy fix for broken XP bar on Retail

--- ConsolePort_Bar/Components/WatchBarMixins.lua
+++ ConsolePort_Bar/Components/WatchBarMixins.lua
@@ -64,6 +64,120 @@ CPExpBarMixin = CreateFromMixins(CPStatusTrackingBarMixin)
 
 local XP_STATUS_BAR_TEXT = 'XP: %d/%d'
 
+local function TextStatusBar_UpdateTextStringWithValues(statusFrame, textString, value, valueMin, valueMax)
+	if( statusFrame.LeftText and statusFrame.RightText ) then
+		statusFrame.LeftText:SetText("");
+		statusFrame.RightText:SetText("");
+		statusFrame.LeftText:Hide();
+		statusFrame.RightText:Hide();
+	end
+	
+	if ( ( tonumber(valueMax) ~= valueMax or valueMax > 0 ) and not ( statusFrame.pauseUpdates ) ) then
+		statusFrame:Show();
+		
+		if ( (statusFrame.cvar and GetCVar(statusFrame.cvar) == "1" and statusFrame.textLockable) or statusFrame.forceShow ) then
+			textString:Show();
+		elseif ( statusFrame.lockShow > 0 and (not statusFrame.forceHideText) ) then
+			textString:Show();
+		else
+			textString:SetText("");
+			textString:Hide();
+			return;
+		end
+
+		local valueDisplay = value;
+		local valueMaxDisplay = valueMax;
+		-- Modern WoW always breaks up large numbers, whereas Classic never did.
+		-- We'll remove breaking-up by default for Classic, but add a flag to reenable it.
+		if ( statusFrame.breakUpLargeNumbers ) then
+			if ( statusFrame.capNumericDisplay ) then
+				valueDisplay = AbbreviateLargeNumbers(value);
+				valueMaxDisplay = AbbreviateLargeNumbers(valueMax);
+			else
+				valueDisplay = BreakUpLargeNumbers(value);
+				valueMaxDisplay = BreakUpLargeNumbers(valueMax);
+			end
+		end
+
+		local textDisplay = GetCVar("statusTextDisplay");
+		if ( value and valueMax > 0 and ( (textDisplay ~= "NUMERIC" and textDisplay ~= "NONE") or statusFrame.showPercentage ) and not statusFrame.showNumeric) then
+			if ( value == 0 and statusFrame.zeroText ) then
+				textString:SetText(statusFrame.zeroText);
+				statusFrame.isZero = 1;
+				textString:Show();
+			elseif ( textDisplay == "BOTH" and not statusFrame.showPercentage) then
+				if( statusFrame.LeftText and statusFrame.RightText ) then
+					if(not statusFrame.powerToken or statusFrame.powerToken == "MANA") then
+						statusFrame.LeftText:SetText(math.ceil((value / valueMax) * 100) .. "%");
+						statusFrame.LeftText:Show();
+					end
+					statusFrame.RightText:SetText(valueDisplay);
+					statusFrame.RightText:Show();
+					textString:Hide();
+				else
+					valueDisplay = "(" .. math.ceil((value / valueMax) * 100) .. "%) " .. valueDisplay .. " / " .. valueMaxDisplay;
+				end
+				textString:SetText(valueDisplay);
+			else
+				valueDisplay = math.ceil((value / valueMax) * 100) .. "%";
+				if ( statusFrame.prefix and (statusFrame.alwaysPrefix or not (statusFrame.cvar and GetCVar(statusFrame.cvar) == "1" and statusFrame.textLockable) ) ) then
+					textString:SetText(statusFrame.prefix .. " " .. valueDisplay);
+				else
+					textString:SetText(valueDisplay);
+				end
+			end
+		elseif ( value == 0 and statusFrame.zeroText ) then
+			textString:SetText(statusFrame.zeroText);
+			statusFrame.isZero = 1;
+			textString:Show();
+			return;
+		else
+			statusFrame.isZero = nil;
+			if ( statusFrame.prefix and (statusFrame.alwaysPrefix or not (statusFrame.cvar and GetCVar(statusFrame.cvar) == "1" and statusFrame.textLockable) ) ) then
+				textString:SetText(statusFrame.prefix.." "..valueDisplay.." / "..valueMaxDisplay);
+			else
+				textString:SetText(valueDisplay.." / "..valueMaxDisplay);
+			end
+		end
+	else
+		textString:Hide();
+		textString:SetText("");
+		if ( not statusFrame.alwaysShow ) then
+			statusFrame:Hide();
+		else
+			statusFrame:SetValue(0);
+		end
+	end
+end
+
+local function TextStatusBar_UpdateTextString(textStatusBar)
+	local textString = textStatusBar.TextString;
+	if(textString) then
+		local value = textStatusBar:GetValue()
+		local valueMin, valueMax = textStatusBar:GetMinMaxValues()
+		TextStatusBar_UpdateTextStringWithValues(textStatusBar, textString, value, valueMin, valueMax)
+	end
+end
+
+local function TextStatusBar_Initialize(self)
+	self:RegisterEvent('CVAR_UPDATE')
+	self.lockShow = 0;
+
+	local function OnStatusTextSettingChanged()
+		TextStatusBar_UpdateTextString(self)
+	end
+
+	Settings.SetOnValueChangedCallback('PROXY_STATUS_TEXT', OnStatusTextSettingChanged)
+end
+
+function CPExpBarMixin:GetValue()
+	return self.StatusBar:GetValue()
+end
+
+function CPExpBarMixin:GetMinMaxValues()
+	return self.StatusBar:GetMinMaxValues()
+end
+
 function CPExpBarMixin:GetPriority()
 	return self.priority 
 end
