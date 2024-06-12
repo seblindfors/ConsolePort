e99b244cb2c8b29ac8f35810c1d6b57faf8474fa
Sebastian Lindfors <sl92cb@gmail.com>
2024-06-13 00:05:08 +0200

    Add support for common drivers in action bar template

--- ConsolePort_Bar/Widget/Bar/Bar.lua
+++ ConsolePort_Bar/Widget/Bar/Bar.lua
@@ -2,53 +2,92 @@ local _, env, db = ...; db = env.db;
 ---------------------------------------------------------------
 CPActionBar = Mixin({
 ---------------------------------------------------------------
-    Env = {
-        OnLoad = [[
-            manager = self:GetFrameRef('Manager')
-            cursor  = manager:GetFrameRef('Cursor')
-            pager   = manager:GetFrameRef('Pager')
-        ]];
-    };
-    -----------------------------------------------------------
+	FadeIn = db.Alpha.FadeIn;
+	Env = {
+		_onshow = [[
+			manager:SetAttribute(self:GetAttribute('signature'), true)
+			manager::RefreshBindings(true)
+		]];
+		_onhide = [[
+			manager:SetAttribute(self:GetAttribute('signature'), false)
+			manager::RefreshBindings(false)
+		]];
+		OnLoad = [[
+			manager = self:GetFrameRef('Manager')
+			cursor  = manager:GetFrameRef('Cursor')
+			pager   = manager:GetFrameRef('Pager')
+		]];
+	};
+	-----------------------------------------------------------
 }, CPAPI.AdvancedSecureMixin, env.CommonWidgetMixin, env.DynamicWidgetMixin);
 ---------------------------------------------------------------
 
 function CPActionBar:OnLoad()
-    db.Pager:RegisterHeader(self)
-    self:SetFrameRef('Manager', env.Manager)
-    self:Run(self.Env.OnLoad)
-    self:EnableMouse(false)
+	db.Pager:RegisterHeader(self)
+	self:SetFrameRef('Manager', env.Manager)
+	self:Run(self.Env.OnLoad)
+	self:EnableMouse(false)
 end
 
 function CPActionBar:RegisterDriver(type, driver, body, current)
-    driver = env.ConvertDriver(driver)
-    body   = CPAPI.ConvertSecureBody(body)
+	driver = env.ConvertDriver(driver)
+	body   = CPAPI.ConvertSecureBody(body)
 
-    RegisterStateDriver(self, type, driver)
-    self:SetAttribute(type, current or SecureCmdOptionParse(driver))
-    self:SetAttribute(env.Attributes.Driver(type), driver)
-    self:SetAttribute(env.Attributes.State(type), body)
-    self:Run([[local newstate = self:GetAttribute(%q) %s]], type, body)
+	RegisterStateDriver(self, type, driver)
+	self:SetAttribute(type, current or SecureCmdOptionParse(driver))
+	self:SetAttribute(env.Attributes.Driver(type), driver)
+	self:SetAttribute(env.Attributes.State(type), body)
+	self:Run([[local newstate = self:GetAttribute(%q) %s]], type, body)
 end
 
 function CPActionBar:RunDriver(type) self:Run([[
-    local newstate = SecureCmdOptionParse(%q); %s
+	local newstate = SecureCmdOptionParse(%q); %s
 ]], self:GetAttribute(env.Attributes.Driver(type)), self:GetAttribute(env.Attributes.State(type))) end
 
 function CPActionBar:RunAttribute(attribute, ...) self:Run([[
-    self::%s(%q)
+	self::%s(%q)
 ]], attribute, ...) end
 
 function CPActionBar:RegisterModifierDriver(driver, body, current)
-    self:RegisterDriver('modifier', driver, body, current)
+	self:RegisterDriver('modifier', driver, body, current)
 end
 
 function CPActionBar:RegisterVisibilityDriver(driver, current)
-    driver = env.ConvertDriver(driver)
-    RegisterStateDriver(self, env.Attributes.Visible, driver)
-    self:SetAttribute(env.Attributes.Visible, current or SecureCmdOptionParse(driver))
+	driver = env.ConvertDriver(driver)
+	RegisterStateDriver(self, env.Attributes.Visible, driver)
+	self:SetAttribute(env.Attributes.Visible, current or SecureCmdOptionParse(driver))
+end
+
+function CPActionBar:UnregisterVisibilityDriver()
+	UnregisterStateDriver(self, env.Attributes.Visible)
 end
 
 function CPActionBar:RegisterPageResponse(body)
-    self:SetAttribute(env.Attributes.OnPage, body)
+	self:SetAttribute(env.Attributes.OnPage, body)
+end
+
+function CPActionBar:OnDriverChanged()
+	-- Driver: visibility
+	self:RegisterVisibilityDriver(self.props.visibility)
+
+	-- Driver: rescale
+	self:RegisterDriver('rescale', env.ConvertDriver(self.props.rescale), [[
+		newstate = (tonumber(newstate) or 100) * 0.01;
+		if newstate > 0 then
+			self:SetScale(newstate)
+		end
+	]])
+
+	-- Driver: opacity
+	self:RegisterDriver('opacity', env.ConvertDriver(self.props.opacity), [[
+		newstate = (tonumber(newstate) or 100) * 0.01;
+		if newstate < 0 then newstate = 0 end;
+		if newstate > 1 then newstate = 1 end;
+		self:CallMethod('FadeIn', 0.05, ALPHA or 0, newstate)
+		ALPHA = newstate;
+	]])
+end
+
+function CPActionBar:OnRelease()
+	self:UnregisterVisibilityDriver()
 end
