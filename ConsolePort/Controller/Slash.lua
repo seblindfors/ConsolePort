859a84bf01a51adb30330662438ef647237ca9ea
Sebastian Lindfors <sl92cb@gmail.com>
2024-11-30 23:21:07 +0100

    Refactor slash handler to provide better documentation

--- ConsolePort/Controller/Slash.lua
+++ ConsolePort/Controller/Slash.lua
@@ -7,10 +7,16 @@ local CURSOR_ADDON_NAME = 'ConsolePort_Cursor';
 -- Process slash command
 ---------------------------------------------------------------
 local function ProcessSlash(command, ...)
-	local func = SLASH_FUNCTIONS[command];
-	if func then
-		func(...)
-		return true
+	local data = SLASH_FUNCTIONS[command];
+	if data then
+		if ( data[1](...) == false ) then
+			SLASH_FUNCTIONS.help[1](command)
+		end
+		return true;
+	elseif command and command:trim():len() > 0 then
+		CPAPI.Log('Unknown command |cFF00FFFF%s|r.', command)
+		SLASH_FUNCTIONS.help[1]()
+		return true;
 	end
 end
 
@@ -93,214 +99,260 @@ SLASH_FUNCTIONS = {
 	-----------------------------------------------------------
 	-- Stack handling
 	-----------------------------------------------------------
-	addframe = function(owner, frame)
-		if owner and frame then
-			local loadable, reason = select(4, CPAPI.GetAddOnInfo(owner))
-			if loadable or CPAPI.IsAddOnLoaded(owner) then
-				CPAPI.EnableAddOn(CURSOR_ADDON_NAME)
-				CPAPI.LoadAddOn(CURSOR_ADDON_NAME)
-				return EventUtil.ContinueOnAddOnLoaded(CURSOR_ADDON_NAME, function()
-					local stack = db.Stack;
-					if stack:TryRegisterFrame(owner, frame, true) then
-						stack:AddFrame(frame)
-						stack:UpdateFrames()
-						return CPAPI.Log('Frame %s was added under %s.', frame, owner)
-					end
-				end)
+	frame = {
+		desc  = 'Add, remove or reset a frame from cursor stack.';
+		usage = {
+			{'add||remove||reset', 'string', 'Action to perform on the frame.'};
+			{'frameName', 'string', 'Name of the frame to add, remove or reset.'};
+			{'[addonName]', 'string', 'Optional name of the addon that owns the frame.'};
+		};
+		function(action, frame, owner)
+			if not owner then owner = CURSOR_ADDON_NAME end;
+			if action and frame then
+				local loadable, reason = select(4, CPAPI.GetAddOnInfo(owner))
+				if loadable or CPAPI.IsAddOnLoaded(owner) then
+					CPAPI.EnableAddOn(CURSOR_ADDON_NAME)
+					CPAPI.LoadAddOn(CURSOR_ADDON_NAME)
+					return EventUtil.ContinueOnAddOnLoaded(CURSOR_ADDON_NAME, function()
+						local stack = db.Stack;
+						if ( action == 'add' ) then
+							if stack:TryRegisterFrame(owner, frame, true) then
+								if stack:AddFrame(frame) then
+									stack:UpdateFrames()
+									return CPAPI.Log('Frame %s was added under %s.', frame, owner)
+								end
+								return CPAPI.Log('Frame %s was registered to %s, but does not exist yet.', frame, owner)
+							end
+						elseif ( action == 'remove' ) then
+							if stack:TryUnregisterFrame(owner, frame) then
+								stack:RemoveFrame(frame)
+								stack:UpdateFrames()
+								return CPAPI.Log('Frame %s was removed from %s.', frame, owner)
+							end
+							return CPAPI.Log('Frame %s was not found in %s. Command ignored.', frame, owner)
+						elseif ( action == 'reset' ) then
+							if stack:TryUnregisterFrame(owner, frame, true) then
+								stack:UpdateFrames()
+								return CPAPI.Log('Frame %s was reset.', frame)
+							end
+							return CPAPI.Log('Frame %s was not found in %s. Nothing to reset.', frame, owner)
+						end
+					end)
+				end
+				return CPAPI.Log('Frame owner %s is not eligble. Reason: %s', owner, reason)
 			end
-			return CPAPI.Log('Addon %s is not eligble. Reason: %s', owner, _G['ADDON_'..reason])
-		end
-		CPAPI.Log(HELP_STRING, 'addframe', 'addonName frameName')
-	end;
-	removeframe = function(owner, frame)
-		if owner and frame then
-			local loadable, reason = select(4, CPAPI.GetAddOnInfo(owner))
-			if loadable or CPAPI.IsAddOnLoaded(owner) then
-				CPAPI.EnableAddOn(CURSOR_ADDON_NAME)
-				CPAPI.LoadAddOn(CURSOR_ADDON_NAME)
-				return EventUtil.ContinueOnAddOnLoaded(CURSOR_ADDON_NAME, function()
-					local stack = db.Stack;
-					if stack:TryRemoveFrame(owner, frame) then
-						stack:RemoveFrame(_G[frame])
-						stack:UpdateFrames()
-						return CPAPI.Log('Frame %s was removed from %s.', frame, owner)
-					end
-				end)
-			end
-			return CPAPI.Log('Addon is not eligble. Reason: %s', reason)
-		end
-		CPAPI.Log(HELP_STRING, 'removeframe', 'addonName frameName')
-	end;
+			return false;
+		end;
+	};
 	-----------------------------------------------------------
 	-- Config
 	-----------------------------------------------------------
-	applyconfig = function(useBluetooth)
-		local bluetooth = useBluetooth and Compile(useBluetooth)
-		local device = db('Gamepad/Active')
-		if device then
-			device:ApplyConfig(bluetooth)
-			CPAPI.Log('Config was applied for your %s device.', device.Name)
-		end
-	end;
-	bluetooth = function(state)
-		if state then
-			local bluetooth = state and Compile(state)
+	status = {
+		desc  = 'Show connected devices.';
+		usage = {
+			{'[deviceID]', 'number', 'Optional device ID to show axis readings and state.'};
+		};
+		function(deviceID)
+			local activeDevices = {};
+			for _, i in ipairs(C_GamePad.GetAllDeviceIDs()) do
+				local device = C_GamePad.GetDeviceRawState(i)
+				local devicePowerLevel = C_GamePad.GetPowerLevel(i)
+				local powerLevelInfo = db.Battery:GetPowerLevelInfo(devicePowerLevel)
+				if device then
+					tinsert(activeDevices, {
+						id    = i;
+						state = device;
+						powerLevel = powerLevelInfo.color:WrapTextInColorCode(powerLevelInfo.name);
+					})
+				end
+			end
+			if next(activeDevices) then
+				CPAPI.Log('Connected devices:')
+				for _, device in ipairs(activeDevices) do
+					local vendorID  = ('%04x'):format(device.state.vendorID):upper();
+					local productID = ('%04x'):format(device.state.productID):upper();
+					local powerLevel = device.powerLevel;
+					local config = C_GamePad.GetConfig({
+						vendorID  = device.state.vendorID;
+						productID = device.state.productID;
+					});
+
+					CPAPI.Log('%d: |cFFFFFFFF%s|r', device.id, device.state.name)
+					CPAPI.Log('   Vendor: |cFF00FFFF%s|r, Product: |cFF00FFFF%s|r, Config: %s, Battery Level: %s',
+						vendorID, productID,
+						config and ('|cFF00FF00%s|r'):format(config.name or 'custom') or '|cFFFFFFFFgeneric|r',
+						powerLevel
+					);
+				end
+			else
+				CPAPI.Log('No connected devices found.')
+			end
+			-- TODO: show axis readings and state when device ID is provided
+		end;
+	};
+	applyconfig = {
+		desc = 'Re-apply config for the active device.';
+		usage = {
+			{'[useBluetooth]', 'bool', 'Optional bluetooth state to apply.'};
+		};
+		function(useBluetooth)
+			local bluetooth = useBluetooth and Compile(useBluetooth)
 			local device = db('Gamepad/Active')
 			if device then
 				device:ApplyConfig(bluetooth)
-				return CPAPI.Log('Bluetooth set to %s for %s.', state, device.Name)
+				CPAPI.Log('Config was applied for your %s device.', device.Name)
 			end
-		end
-		CPAPI.Log(HELP_STRING, 'bluetooth', 'true/false')
-	end;
-	config = function(path, value)
-		if not path and not value then
-			return CPAPI.Log(HELP_STRING, 'config', 'path/to/key [value]')
-		end
-
-		local config = db('Gamepad/Active/Config')
-		if not config then return CPAPI.Log('No active config found.') end
-
-		if not value then
-			value = db('Gamepad/Active/Config/'..path)
-			if value then
-				CPAPI.Log('Value found for %s:', path)
-				DevTools_Dump(value)
-			else
-				CPAPI.Log('Value not found for %s.', path)
+		end;
+	};
+	bluetooth = {
+		desc = 'Change bluetooth state for active device.';
+		usage = {
+			{'state', 'bool', 'State to set bluetooth to.'};
+		};
+		function(state)
+			if state then
+				local bluetooth = state and Compile(state)
+				local device = db('Gamepad/Active')
+				if device then
+					device:ApplyConfig(bluetooth)
+					return CPAPI.Log('Bluetooth set to %s for %s.', state, device.Name)
+				end
 			end
-			return
-		end
-
-		local newstate = Compile(value)
-		local __mt, __index = {};
-		function __index(t, k)
-			local v = setmetatable({}, __mt)
-			rawset(t, k, v)
-			return v;
-		end
-		__mt.__index = __index;
-
-		setmetatable(config, __mt)
-		if db('Gamepad/Active/Config/'..path, newstate) then
-			db('Gamepad/Active'):ApplyConfig()
-			return CPAPI.Log('Value %s set at %s.', tostring(newstate), path)
-		end
-	end;
-	status = function(deviceID)
-		local activeDevices = {};
-		for _, i in ipairs(C_GamePad.GetAllDeviceIDs()) do
-			local device = C_GamePad.GetDeviceRawState(i)
-			local devicePowerLevel = C_GamePad.GetPowerLevel(i)
-			local powerLevelInfo = db.Battery:GetPowerLevelInfo(devicePowerLevel)
-			if device then
-				tinsert(activeDevices, {
-					id    = i;
-					state = device;
-					powerLevel = powerLevelInfo.color:WrapTextInColorCode(powerLevelInfo.name);
-				})
+			return false;
+		end;
+	};
+	config = {
+		desc = 'Change or print a value from the active device configuration.';
+		usage = {
+			{'path/to/key', 'string', 'Path to the key in the configuration.'};
+			{'[value]', 'any', 'Optional value to set at the key.'};
+		};
+		function(path, value)
+			if not path and not value then
+				return false;
 			end
-		end
-		if next(activeDevices) then
-			CPAPI.Log('Connected devices:')
-			for _, device in ipairs(activeDevices) do
-				local vendorID  = ('%04x'):format(device.state.vendorID):upper();
-				local productID = ('%04x'):format(device.state.productID):upper();
-				local powerLevel = device.powerLevel;
-				local config = C_GamePad.GetConfig({
-					vendorID  = device.state.vendorID;
-					productID = device.state.productID;
-				});
 
-				CPAPI.Log('%d: |cFFFFFFFF%s|r', device.id, device.state.name)
-				CPAPI.Log('   Vendor: |cFF00FFFF%s|r, Product: |cFF00FFFF%s|r, Config: %s, Battery Level: %s',
-					vendorID, productID,
-					config and ('|cFF00FF00%s|r'):format(config.name or 'custom') or '|cFFFFFFFFgeneric|r',
-					powerLevel
-				);
+			local config = db('Gamepad/Active/Config')
+			if not config then return CPAPI.Log('No active config found.') end
+
+			if not value then
+				value = db('Gamepad/Active/Config/'..path)
+				if value then
+					CPAPI.Log('Value found for %s:', path)
+					DevTools_Dump(value)
+				else
+					CPAPI.Log('Value not found for %s.', path)
+				end
+				return
 			end
-		else
-			CPAPI.Log('No connected devices found.')
-		end
-		-- TODO: show axis readings and state when device ID is provided
-	end;
+
+			local newstate = Compile(value)
+			local __mt, __index = {};
+			function __index(t, k)
+				local v = setmetatable({}, __mt)
+				rawset(t, k, v)
+				return v;
+			end
+			__mt.__index = __index;
+
+			setmetatable(config, __mt)
+			if db('Gamepad/Active/Config/'..path, newstate) then
+				db('Gamepad/Active'):ApplyConfig()
+				return CPAPI.Log('Value %s set at %s.', tostring(newstate), path)
+			end
+		end;
+	};
+	resetconfigs = {
+		desc  = 'Reset all mapping configurations and reload. (will not affect bindings)';
+		function()
+			for i, config in ipairs(C_GamePad.GetAllConfigIDs()) do
+				C_GamePad.DeleteConfig(config)
+			end
+			C_GamePad.ApplyConfigs()
+			ReloadUI()
+		end;
+	};
 	-----------------------------------------------------------
 	-- Reset/uninstall
 	-----------------------------------------------------------
-	clearbindings = function()
-		ClearPadBindings()
-		ReloadUI()
-	end;
-	resetall = function()
-		CPAPI.Popup('ConsolePort_Uninstall_Settings', {
-			text = 'This action will remove all your saved settings and reload your interface.';
-			button1 = OKAY;
-			button2 = CANCEL;
-			timeout = 0;
-			whileDead = 1;
-			showAlert = 1;
-			OnAccept = Uninstall;
-		})
-	end;
-	resetconfigs = function()
-		for i, config in ipairs(C_GamePad.GetAllConfigIDs()) do
-			C_GamePad.DeleteConfig(config)
-		end
-		C_GamePad.ApplyConfigs()
-		ReloadUI()
-	end;
-	uninstall = function()
-		CPAPI.Popup('ConsolePort_Uninstall_Addon', {
-			text = 'This action will remove all your saved settings and reload your interface.';
-			button1 = OKAY;
-			button2 = CANCEL;
-			timeout = 0;
-			whileDead = 1;
-			showAlert = 1;
-			OnAccept = function()
-				CPAPI.DisableAddOn('ConsolePort')
-				SetCVar('GamePadEnable', 0)
-				ClearPadBindings()
-				Uninstall()
-			end;
-		})
-	end;
+	clearbindings = {
+		desc = 'Clear configured gamepad bindings and reload interface.';
+		function()
+			ClearPadBindings()
+			ReloadUI()
+		end;
+	};
+	resetall = {
+		desc = 'Remove all saved settings and reload interface.';
+		function()
+			CPAPI.Popup('ConsolePort_Uninstall_Settings', {
+				text = 'This action will remove all your saved settings and reload your interface.';
+				button1 = OKAY;
+				button2 = CANCEL;
+				timeout = 0;
+				whileDead = 1;
+				showAlert = 1;
+				OnAccept = Uninstall;
+			})
+		end;
+	};
+	uninstall = {
+		desc = 'Remove all saved settings and bindings, disable addon, and reload interface.';
+		function()
+			CPAPI.Popup('ConsolePort_Uninstall_Addon', {
+				text = 'This action will remove all your saved settings and reload your interface.';
+				button1 = OKAY;
+				button2 = CANCEL;
+				timeout = 0;
+				whileDead = 1;
+				showAlert = 1;
+				OnAccept = function()
+					CPAPI.DisableAddOn('ConsolePort')
+					SetCVar('GamePadEnable', 0)
+					ClearPadBindings()
+					Uninstall()
+				end;
+			})
+		end;
+	};
 	-----------------------------------------------------------
 	-- Help
 	-----------------------------------------------------------
-	help = function(command)
-		local commands = {
-			{'<empty>', '',
-				'Open configuration interface.'};
-			{'addframe', 'addonName frameName',
-				'Add a custom frame to cursor stack.'};
-			{'removeframe', 'addonName frameName',
-				'Remove a frame from cursor stack.'};
-			{'applyconfig', 'useBluetooth',
-				'Apply config for the active device.'};
-			{'bluetooth', 'true/false',
-				'Change bluetooth state for active device.'};
-			{'config', 'path/to/key [value]',
-				'Change or print a value from the active device configuration.'};
-			{'status', '[deviceID]',
-				'Show connected devices.'};
-			{'clearbindings', '',
-				'Clear configured gamepad bindings and reload interface.'};
-			{'resetall', '',
-				'Remove all saved settings and reload interface.'};
-			{'resetconfigs', '',
-				'Reset all mapping configurations and reload. (will not affect bindings)'};
-			{'uninstall', '',
-				'Remove all saved settings and bindings, disable addon, and reload interface.'};
-		}
-		if not command then
-			CPAPI.Log(HELP_STRING, 'command', '[args]')
-			for i, command in ipairs(commands) do
-				CPAPI.Log(DOCU_STRING, unpack(command))
+	help = {
+		desc = 'Show help for command(s).';
+		usage = {
+			{'[command]', 'string', 'Optional command to show help for.'};
+		};
+		function(command)
+			local function GenerateUsage(data, usage) usage = usage or '';
+				if data.usage then
+					local args = {};
+					for _, arg in ipairs(data.usage) do
+						tinsert(args, arg[1])
+					end
+					usage = table.concat(args, ' ')
+				end
+				return usage;
 			end
-		end
-	end;
-}
+
+			local data = SLASH_FUNCTIONS[command];
+			if data then
+				CPAPI.Log(HELP_STRING, command, GenerateUsage(data))
+				if data.usage then
+					for _, arg in ipairs(data.usage) do
+						CPAPI.Log(DOCU_STRING, unpack(arg))
+					end
+				end
+			else
+				CPAPI.Log(HELP_STRING, 'command', '[args]')
+				CPAPI.Log(DOCU_STRING, '<empty>', '', 'Open configuration interface.')
+				for func, data in db.table.spairs(SLASH_FUNCTIONS) do
+					CPAPI.Log(DOCU_STRING, func, GenerateUsage(data), data.desc)
+				end
+			end
+		end;
+	}
+};
 
 ---------------------------------------------------------------
 -- Set up slash handler
