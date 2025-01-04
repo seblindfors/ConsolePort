8b6b4fc72bf9e3d87377f95b39188d8d6abf5516
Sebastian Lindfors <sl92cb@gmail.com>
2025-01-04 17:03:55 +0100

    Fix bug causing global hint bar to not use ConsolePort prefix in name
    - Also add timer support to button prompts (for later use)

--- ConsolePort/Widget/HintBar/HintBar.lua
+++ ConsolePort/Widget/HintBar/HintBar.lua
@@ -59,6 +59,27 @@ function CPHintMixin:SetData(icon, text)
 	self:UpdateParentWidth()
 end
 
+function CPHintMixin:SetTimer(duration)
+	local timer = self:GetTimer(true)
+	CooldownFrame_Set(timer, GetTime(), duration, 1, true)
+end
+
+function CPHintMixin:ClearTimer()
+	local timer = self:GetTimer(false)
+	if timer then
+		CooldownFrame_Clear(self:GetTimer())
+	end
+end
+
+function CPHintMixin:GetTimer(create)
+	if not self.timer then
+		if not create then return end;
+		self.timer = CreateFrame('Cooldown', nil, self, 'CPHintTimerTemplate')
+		self.timer:SetPoint('CENTER', self.Icon, 'CENTER', 0, 0)
+	end
+	return self.timer;
+end
+
 ----------------------------------
 -- HintBar mixin
 ----------------------------------
